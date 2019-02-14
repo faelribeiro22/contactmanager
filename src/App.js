b1e43ae5c6566d505cf54f5fe57fe26f8d7216de
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2019-02-14 21:49:42 -0200

    fix: End a module form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import Contacts from './components/Contacts';
-import Header from './components/Header';
+import Contacts from './components/contacts/Contacts';
+import Header from './components/layout/Header';
+import AddContact from './components/contacts/AddContact';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Provider } from './context';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <div className="container">
-          <Contacts />
+      <Provider>
+        <div className="App">
+          <Header />
+          <div className="container">
+            <AddContact/>
+            <Contacts />
+          </div>
         </div>
-      </div>
+      </Provider>
     );
   }
 }
